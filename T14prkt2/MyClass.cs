a0e89a4651e708c8853f5c8a6c04126ba633da67
artem-mutaf <123882882+artem-mutaf@users.noreply.github.com>
2024-01-22 09:47:48 +0200

    Add files via upload

---
+++ T14prkt2/MyClass.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T14prkt2
+{
+    class MyClass
+    {
+        double value1, value2;
+        public static int count;
+        public MyClass(double value1,double value2)
+        {
+            this.value1 = value1;
+            this.value2 = value2;
+            count++;
+        }
+        public MyClass()
+        {
+            value1 = 2.0;
+            value2 = 2.0;
+            count++;
+        }
+        static MyClass()
+        {
+            count = 0;
+        }
+        public double Value1
+        {
+            get { return value1; }
+            set { value1 = value; }
+        }
+        public double Value2
+        {
+            get { return value2; }
+            set { value2 = value; }
+        }
+        public override string ToString()
+        {
+            return $"{value1}\t{value2}\n{count}";
+        }
+
+    }
+}
---
+++ T14prkt2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T14prkt2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass mcl = new MyClass();
+            Console.WriteLine(mcl.ToString());
+            MyClass mcl1 = new MyClass(8,90);
+            Console.WriteLine(mcl1.ToString());
+
+            MyClass[] points = new MyClass[4];
+            for(int i = 1;i<points.Length;i++)
+            {
+                Console.WriteLine($"Введите координаты {i} точки ");
+                Console.Write("x= ");
+                double x = double.Parse(Console.ReadLine());
+                Console.Write("y= ");
+                double y = double.Parse(Console.ReadLine());
+                
+            }
+            Console.Write(points.ToString());
+            Console.ReadKey();
+            
+
+        }
+        
+    }
+}
